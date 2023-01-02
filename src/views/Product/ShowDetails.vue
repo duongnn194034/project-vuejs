13be511d5fa63ccf863fbe97dabf8f7b2541dbdb
duongnn <laserbean4@gmail.com>
2023-01-02 22:48:52 +0700

    small adjust

--- src/views/Product/ShowDetails.vue
+++ src/views/Product/ShowDetails.vue
@@ -37,11 +37,7 @@
         <div class="features pt-3">
           <h5><strong>Features</strong></h5>
           <ul>
-            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-            <li>Officia quas, officiis eius magni error magnam voluptatem</li>
-            <li>nesciunt quod! Earum voluptatibus quaerat dolorem doloribus</li>
-            <li>molestias ipsum ab, ipsa consectetur laboriosam soluta et</li>
-            <li>ut doloremque dolore corrupti, architecto iusto beatae.</li>
+            <li>Details</li>
           </ul>
         </div>
 
