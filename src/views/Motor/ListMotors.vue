6035622000baf133fe01800bfdf0e8e7b4d10a19
duongnn <laserbean4@gmail.com>
2023-05-29 23:16:47 +0700

    Time select

--- src/views/Motor/ListMotors.vue
+++ src/views/Motor/ListMotors.vue
@@ -10,30 +10,33 @@
           <DatePicker v-model="pickUp">
             <template #default="{ togglePopover }">
               <input
-                class="form-control datetime-input-1"
+                class="form-control datetime-input-1 col-md-4"
                 type="text"
-                placeholder="Pick-up time"
+                placeholder="Pick-up date"
                 :value="pickUp?.toLocaleDateString()"
                 readonly
                 @click="togglePopover"
               >
             </template>
           </DatePicker>
-          <select class="form-control text-center" v-model="type" required>
+          <select class="form-control text-center col-md-2" v-model="puTime">
             <option v-for="value in timestamp" :value="value">{{ value }}</option>
           </select>
-          <DatePicker v-model="dropOff" mode="datetime">
+          <DatePicker v-model="dropOff">
             <template #default="{ togglePopover }">
               <input
-                class="form-control datetime-input-2"
+                class="form-control datetime-input-2 col-md-4"
                 type="text"
-                placeholder="Drop-off time"
+                placeholder="Drop-off date"
                 :value="dropOff?.toLocaleDateString()"
                 readonly
                 @click="togglePopover"
               >
             </template>
           </DatePicker>
+          <select class="form-control text-center col-md-2" v-model="doTime">
+            <option v-for="value in timestamp" :value="value">{{ value }}</option>
+          </select>
         </div>
       </div>
     </div>
@@ -67,6 +70,8 @@ export default {
       query: null,
       pickUp: null,
       dropOff: null,
+      puTime: null,
+      doTime: null,
       timestamp: ['06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30',
     '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00',
     '21:30', '22:00', '22:30', '23:00'],
