d899c5c1cb8e9664e62b759f0f2f2624f6a1e625
Nguyen Nam Duong <DuongNNb@bkav.com>
2023-05-29 12:09:21 +0700

    temp

--- src/views/Motor/ListMotors.vue
+++ src/views/Motor/ListMotors.vue
@@ -19,7 +19,10 @@
               >
             </template>
           </DatePicker>
-          <DatePicker v-model="dropOff">
+          <select class="form-control text-center" v-model="type" required>
+            <option v-for="value in timestamp" :value="value">{{ value }}</option>
+          </select>
+          <DatePicker v-model="dropOff" mode="datetime">
             <template #default="{ togglePopover }">
               <input
                 class="form-control datetime-input-2"
@@ -64,6 +67,9 @@ export default {
       query: null,
       pickUp: null,
       dropOff: null,
+      timestamp: ['06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30',
+    '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00',
+    '21:30', '22:00', '22:30', '23:00'],
     }
   },
   props : [ "baseURL" ],
